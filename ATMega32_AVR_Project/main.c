4b79f778f4c3e37e89cccdf3d20e0763041a6b9f
Mohamedmukhtar1 <muhamdmokhtar1@gmail.com>
2022-05-06 15:26:31 +0200

    Microchip project

---
+++ ATMega32_AVR_Project/main.c
@@ -0,0 +1,18 @@
+/*
+ * ATMega32_AVR_Project.c
+ *
+ * Created: 2022-05-06 03:25:40 PM
+ * Author : Eng- Mohamed Mokhtar
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
