ceb7fee747928d06ec717184d29fcac791c12630
Mohamedmukhtar1 <muhamdmokhtar1@gmail.com>
2022-05-07 21:46:09 +0200

    ADC 70

--- ATMega32_AVR_Project/MCAL/ADC/MCAL_ADC.h
+++ ATMega32_AVR_Project/MCAL/ADC/MCAL_ADC.h
@@ -33,20 +33,32 @@
 #define PRESCALER_DIV_FACTOR_64_	6		// for 16 MHz/64  = 250 KHz
 #define PRESCALER_DIV_FACTOR_128_	7		// for 16 MHz/128 = 125 KHz
 
-#define ADC_CHANNEL_0_		(U8)0	//(default)
-#define ADC_CHANNEL_1_		(U8)1
-#define ADC_CHANNEL_2_		(U8)2
-#define ADC_CHANNEL_3_		(U8)3
-#define ADC_CHANNEL_4_		(U8)4
-#define ADC_CHANNEL_5_		(U8)5
-#define ADC_CHANNEL_6_		(U8)6
-#define ADC_CHANNEL_7_		(U8)7
+#define ADC_CHANNEL_0		(U8)0	//(default)
+#define ADC_CHANNEL_1		(U8)1
+#define ADC_CHANNEL_2		(U8)2
+#define ADC_CHANNEL_3		(U8)3
+#define ADC_CHANNEL_4		(U8)4
+#define ADC_CHANNEL_5		(U8)5
+#define ADC_CHANNEL_6		(U8)6
+#define ADC_CHANNEL_7		(U8)7
 							//76543210		REFS1 REFS0 ADLAR  MUX4  MUX3  MUX2  MUX1  MUX0
 #define ADMUX_SAFE_RANGE	0b11100000	 //	  1	    1	  1		0	  0		0	  0		0
 #define ADMUX_WRITE_RANGE	0b00011111   //   0	    0	  0		1	  1		1     1		1
+
+#define  TRIGGERING_DISABLE_MODE								0
+#define  TRIGGERING_SOURCE_FREE_RUNNING_MODE					1
+#define  TRIGGERING_SOURCE_ANALOG_COMPARATOR					2
+#define  TRIGGERING_SOURCE_EXT_INTERRUPT_0_REQUEST				3
+#define  TRIGGERING_SOURCE_TIMER_0_COMPARE_MATCH				4
+#define  TRIGGERING_SOURCE_TIMER_0_OVERFLOW						5
+#define  TRIGGERING_SOURCE_TIMER_1_COMPARE_MATCH_B				6
+#define  TRIGGERING_SOURCE_TIMER_1_OVERFLOW						7
+#define  TRIGGERING_SOURCE_TIMER_1_CAPTURE_EVENT				8
 /***************_MCAL_ADC.c_FUNCTION_PROTOTYPE_************/
 void MCAL_ADC_INITIATE(U8 Volt_Referencex);
 void MCAL_ADC_Disable(void);
 void MCAL_ADC_Channel_Select(U8 ADC_Channel_Name);
+void MCAL_ADC_Auto_Trigering_Mode(U8 Auto_Trigering_Conig);
+
 
 #endif /* MCAL_ADC_H_ */
